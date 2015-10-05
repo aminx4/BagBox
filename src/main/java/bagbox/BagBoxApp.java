03187826fc8d5ae6aa0ca5747658d6d93e692be8
Amine CHIKOUCHE <amine.chikouche@gmail.com>
2015-10-05 17:47:44 +0200

    second commit !

--- src/main/java/bagbox/BagBoxApp.java
+++ src/main/java/bagbox/BagBoxApp.java
@@ -1,21 +1,43 @@
 package bagbox;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import bagbox.beans.User;
+import bagbox.repository.IUserRepository;
+
 @RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class BagBoxApp {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
+	@Autowired
+	IUserRepository userRepository;
+	
+	
+	@RequestMapping("/authentication")
+	public Map<String,Object> home() {
+		Map<String,Object> model = new HashMap<String,Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<User> getUsers(){
+		return userRepository.findAll();
+	}
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(BagBoxApp.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BagBoxApp.class, args);
+	}
 
 }
